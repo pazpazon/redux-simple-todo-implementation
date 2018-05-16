23defb7a62bcf866f41ba9ebc0a9c72e473e5c76
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-05-16 13:16:04 +0200

    moving along with implementation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function createStore (){
+function createStore (reducer){
   // 1. the state
   // 2. get the state
   // 3. listen to changes
@@ -9,21 +9,21 @@ function createStore (){
 
   const getState = () => state
 
-  const subcribe = (listener) => {
+  const subscribe = (listener) => {
     listeners.push(listener)
-    return () => listeners = listers.filter((fn) => fn !== listener)
+    return () => listeners = listers.filter((l) => l !== listener)
   }
 
   const dispatch = (action) => {
     // call reducer
-    state = todos(state, action)
-    // invoke listeners w. new state
+    state = reducer(state, action)
+    // invoke listeners
     listeners.forEach((listener) => listener())
   }
 
   return {
     getState,
-    subscirbe,
+    subscribe,
     dispatch
   }
 }
@@ -35,10 +35,51 @@ Characteristics of a pure function:
 3. never produce any side effects
 */
 
-function todos(state = [], action){
-  if (action.type === 'ADD_TODO'){
-    return state.concat([action.todo])
-  }
+//Reducer functions
 
-  return state
+//actions:
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function todos(state = [], action){
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+
+    case REMOVE_TODO:
+      return state.filter(item => item.id !== action.id);
+
+    case TOGGLE_TODO:
+      return state.map(item => (item.id !== action.id ? item : Object.assign(
+                {},
+                item,
+                { complete: !item.complete }
+              )));
+
+    default:
+      return state;
+  }
+}
+
+function goals(state = [], action){
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+
+    case REMOVE_GOAL:
+      return state.filter(item => item.id !== action.id);
+
+    default:
+      return state;
+  }
+}
+
+function App(state = {}, action){
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action)
+  }
 }
